d2b61ca7c65e735880b4f18789704146c57902cf
lliao168 <louiseliao0802@gmail.com>
2024-03-28 12:52:18 -0400

    a6 updates

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 // const express = require('express');
 import express from "express";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
-
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+import "dotenv/config";
+import session from "express-session";
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+      })
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }  
+  app.use(
+    session(sessionOptions)
+  );  
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
