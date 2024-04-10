c38d18d2bdc71dc6bd6eb9196dd359105684cd0d
lliao168 <louiseliao0802@gmail.com>
2024-04-09 23:33:40 -0400

    a6 updates

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 // const express = require('express');
 import express from "express";
 import mongoose from "mongoose";
-import UserRoutes from "./Users/routes.js";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
-mongoose.connect("mongodb://localhost:27017/kanbas");
+const DB_CONNECTION_STRING="mongodb+srv://lliao26:2358901Bingo@cluster0.vekd8xb.mongodb.net/kanbas?retryWrites=true&w=majority&appName=Cluster0";
+const CONNECTION_STRING = DB_CONNECTION_STRING || "mongodb://localhost:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+import UserRoutes from "./Users/routes.js";
 import "dotenv/config";
 import session from "express-session";
 const app = express();
